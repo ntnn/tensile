28ea5da47406c91c8e5dff6e669cd3c65cf9dd6d
Nelo-T. Wallus <nelo@wallus.de>
2023-03-10 19:58:37 +0100

    Update custom_shape

--- examples/custom_shape/custom_shape.go
+++ examples/custom_shape/custom_shape.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/ntnn/tensile"
 	"github.com/ntnn/tensile/engines"
@@ -10,19 +11,38 @@ import (
 )
 
 const (
+	// This is a custom shape that will be recognized by queues and
+	// engines.
+	// Custom shapes are useful for e.g. implementing custom resources
+	// like uci for OpenWRT.
 	MyShape tensile.Shape = "myshape"
 )
 
-type MyElement struct {
+// Two nodes utilizing the custom shape.
+type MyNodeA struct {
 	Message string
 }
 
-func (my MyElement) Identity() (tensile.Shape, string) {
+func (my MyNodeA) Identity() (tensile.Shape, string) {
 	return MyShape, my.Message
 }
 
-func (my MyElement) Execute(ctx context.Context) error {
-	log.Println(my.Message)
+func (my MyNodeA) Execute(ctx tensile.Context) error {
+	ctx.Logger(my).Info(my.Message)
+	return nil
+}
+
+type MyNodeB struct {
+	Message   string
+	Timestamp time.Time
+}
+
+func (my MyNodeB) Identity() (tensile.Shape, string) {
+	return MyShape, my.Message
+}
+
+func (my MyNodeB) Execute(ctx tensile.Context) error {
+	ctx.Logger(my).Info(my.Message, slog.Time("timestamp", my.Timestamp))
 	return nil
 }
 
@@ -36,8 +56,15 @@ func doMain() error {
 	simple := engines.NewSimple(slog.Default())
 
 	if err := simple.Queue.Add(
-		&MyElement{
+		&MyNodeA{
 			Message: "Hello, world!",
+		},
+		&MyNodeB{
+			// This would cause an error - MyNodeA and MyNodeB are
+			// different nodes but both have the custom shape MyShape
+			// and the same message, as such they would conflict.
+			// Message: "Hello, world!",
+			Message: "Hello, world 2!",
 		},
 	); err != nil {
 		return err
