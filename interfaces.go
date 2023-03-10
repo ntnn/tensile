e82f5e6fa291d84673fd438b9b0684161c0685fe
Nelo-T. Wallus <nelo@wallus.de>
2023-03-10 01:15:06 +0100

    Add docs

--- interfaces.go
+++ interfaces.go
@@ -1,27 +1,41 @@
 package tensile
 
 import (
-	"context"
 	"fmt"
 )
 
-// interfaces commonly used by engines
+// interfaces commonly used in tensile
 
+// Validator validates an element when adding it to a queue.
+// The error will be returned by the queue if validation fails.
+//
+// This can be used to e.g. validate that all required options are set
+// or to set internal values.
 type Validator interface {
 	Validate() error
 }
 
+// Identitier returns the full Identity of an element.
 type Identitier interface {
 	Identity() (Shape, string)
 }
 
+// FormatIdentitier formats the identity of an Identitier.
 func FormatIdentitier(ident Identitier) string {
 	shape, name := ident.Identity()
 	return fmt.Sprintf("%s[%s]", shape, name)
 }
 
+type NeedsExecutioner interface {
+	NeedsExecution(Context) (bool, error)
+}
+
+// Executor executes the element.
 type Executor interface {
-	Execute(context.Context) error
+	// Execute executes the element.
+	// The return value can be a struct to be utilized by other
+	// elements.
+	Execute(Context) (any, error)
 }
 
 type PreElementer interface {
