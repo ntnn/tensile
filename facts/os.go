7850c89a15b14a54d51a293e0736b6defb0ed51e
Nelo-T. Wallus <nelo@wallus.de>
2023-03-12 22:20:15 +0100

    Implement reading os-release

---
+++ facts/os.go
@@ -0,0 +1,26 @@
+package facts
+
+// OSRelease is filled from the os-release file where available. On
+// hosts without an equivalent file a best effort is done to fill the
+// same fields.
+type OSRelease struct {
+	Name       string `osrel:"NAME" json:"name"`
+	PrettyName string `osrel:"PRETTY_NAME" json:"pretty_name"`
+
+	CPEName string `osrel:"CPE_NAME" json:"cpe_name"`
+
+	ID     string   `osrel:"ID" json:"id"`
+	IDLike []string `osrel:"ID_LIKE" json:"id_like"`
+
+	Variant   string `osrel:"VARIANT" json:"variant"`
+	VariantID string `osrel:"VARIANT_ID" json:"variant_id"`
+
+	Version         string `osrel:"VERSION" json:"version"`
+	VersionID       string `osrel:"VERSION_ID" json:"version_id"`
+	VersionCodename string `osrel:"VERSION_CODENAME" json:"version_codename"`
+
+	BuildID string `osrel:"BUILD_ID" json:"build_id"`
+
+	ImageID      string `osrel:"IMAGE_ID" json:"image_id"`
+	ImageVersion string `osrel:"IMAGE_VERSION" json:"image_version"`
+}
---
+++ facts/os_default_test.go
@@ -0,0 +1,36 @@
+package facts
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewOSReleaseFileWithPath(t *testing.T) {
+	s, err := os.MkdirTemp("", "tensile-facts-test")
+	require.Nil(t, err)
+	defer func() { require.Nil(t, os.RemoveAll(s)) }()
+
+	p := filepath.Join(s, "os-release")
+
+	exampleData := `NAME=testdistro
+PRETTY_NAME="pretty distro name"
+CPE_NAME=cpe:/o:distro:name:5
+ID_LIKE=distroFamily1 distroFamily2
+VERSION=""
+VERSION_ID=
+`
+
+	require.Nil(t, os.WriteFile(p, []byte(exampleData), 0600))
+
+	rel, err := newOSReleaseFileWithPath(p)
+	require.Nil(t, err)
+	require.Equal(t, "testdistro", rel.Name)
+	require.Equal(t, "pretty distro name", rel.PrettyName)
+	require.Equal(t, "cpe:/o:distro:name:5", rel.CPEName)
+	require.Equal(t, []string{"distroFamily1", "distroFamily2"}, rel.IDLike)
+	require.Equal(t, "", rel.Version)
+	require.Equal(t, "", rel.VersionID)
+}
