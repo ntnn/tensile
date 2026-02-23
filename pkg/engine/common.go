48a1b1cd8aad3df45e3478c51f5b7c2437e2607f
Nelo-T. Wallus <red.brush9525@fastmail.com>
2026-02-23 23:11:16 +0100

    Add engine
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>

---
+++ pkg/engine/common.go
@@ -0,0 +1,28 @@
+package engine
+
+import "log/slog"
+
+// Options are common options for all execution engines.
+type Options struct {
+	// Logger is the logger to use for logging.
+	Logger *slog.Logger
+
+	// Noop prevents execution of any actions. Instead the engine will
+	// perform a dry run.
+	Noop bool
+}
+
+func (o Options) WithDefaults() Options {
+	if o.Logger == nil {
+		o.Logger = slog.Default()
+	}
+	return o
+}
+
+// Summary is the summary of an execution.
+type Summary struct {
+	// NodesExecuted is the number of nodes that were executed.
+	// In noop mode this indicates the number of nodes that would have
+	// been executed if noop were disabled.
+	NodesExecuted int
+}
---
+++ pkg/engine/sequential.go
@@ -0,0 +1,76 @@
+package engine
+
+import (
+	"fmt"
+
+	"github.com/ntnn/tensile"
+	"github.com/ntnn/tensile/pkg/queue"
+)
+
+// Sequential is a simple execution engine that executes nodes in the
+// order the work queue yields them without parallelisation. If any node
+// errors the execution is stopped and the error is returned.
+type Sequential struct {
+	opts Options
+
+	work    *queue.Work
+	summary *Summary
+}
+
+// quential creates a new Sequential execution engine.
+func NewSequential(work *queue.Work, opts Options) *Sequential {
+	s := new(Sequential)
+	s.opts = opts.WithDefaults()
+	s.work = work
+	s.summary = &Summary{}
+	return s
+}
+
+func (s *Sequential) Summary() *Summary {
+	return s.summary
+}
+
+// Execute executes the nodes in the work queue.
+func (s *Sequential) Execute() error {
+	s.opts.Logger.Info("starting sequential engine")
+	for {
+		s.opts.Logger.Info("getting next node from work queue")
+		node, done := s.work.Get()
+		if done {
+			s.opts.Logger.Info("all nodes are done, stopping")
+			return nil
+		}
+
+		if err := s.executeNode(node); err != nil {
+			return err
+		}
+	}
+}
+
+func (s *Sequential) executeNode(node *tensile.Node) error {
+	needsExecution, err := node.NeedsExecution()
+	if err != nil {
+		return fmt.Errorf("failed to check if node with ID %d needs execution: %w", node.ID(), err)
+	}
+	if !needsExecution {
+		s.opts.Logger.Info(fmt.Sprintf("node with ID %d does not need execution, marking as done", node.ID()))
+		s.work.MarkDone(node)
+		return nil
+	}
+
+	if s.opts.Noop {
+		s.opts.Logger.Info(fmt.Sprintf("noop is enabled, skipping execution of node with ID %d", node.ID()))
+		s.work.MarkDone(node)
+		s.summary.NodesExecuted++
+		return nil
+	}
+
+	if err := node.Execute(); err != nil {
+		return fmt.Errorf("failed to execute node with ID %d: %w", node.ID(), err)
+	}
+
+	s.opts.Logger.Info(fmt.Sprintf("successfully executed node with ID %d", node.ID()))
+	s.work.MarkDone(node)
+	s.summary.NodesExecuted++
+	return nil
+}
