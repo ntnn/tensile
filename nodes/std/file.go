fe5d24aa128a6ad3dc7fb695d75f94624ef1856d
Nelo-T. Wallus <red.brush9525@fastmail.com>
2026-09-24 03:55:11 +0200

    Drop tensile.Graph and tensile.Group
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>

--- nodes/std/file.go
+++ nodes/std/file.go
@@ -4,11 +4,17 @@ import (
 	"os"
 
 	"github.com/ntnn/tensile"
+	"github.com/ntnn/tensile/pkg/queue"
 )
 
 // FileIdentity returns the identity of the node managing the file at path.
 func FileIdentity(path string) tensile.Identity {
 	return tensile.AsIdentity("file", "path", path)
+}
+
+// FileQueueIdentity returns the identity of the queue managing the file at path.
+func FileQueueIdentity(path string) tensile.Identity {
+	return tensile.AsIdentity("queue", "file", path)
 }
 
 // File describes a file with ownership and permissions.
@@ -20,15 +26,14 @@ type File struct {
 	Content  string
 }
 
-// NewFile returns a [tensile.Group] managing a file.
-func NewFile(file File) (*tensile.Group, error) {
+// NewFile returns a named queue managing a file.
+func NewFile(file File) *queue.NamedQueue {
+	q := queue.NewNamed(FileQueueIdentity(file.Path))
 	content := &FileContent{Path: file.Path, Content: file.Content}
 	chmod := Chmod{Path: file.Path, FileMode: file.FileMode}
 	chown := Chown{Path: file.Path, Owner: file.Owner, Group: file.Group}
-
-	group := tensile.NewGroup("file " + file.Path)
-	if err := group.Add(content, chmod, chown); err != nil {
-		return nil, err
-	}
-	return group, nil
+	q.Add(content, chmod, chown)
+	// [FileContent] creates the file if it does not exist, chmod/chown do not
+	q.RequiredBy(content, chmod, chown)
+	return q
 }
