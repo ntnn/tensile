7344aa838d20bcfe73ec88da5987a4a3a3f57621
Nelo-T. Wallus <red.brush9525@fastmail.com>
2026-09-26 17:22:07 +0200

    Always print nodes in summary with outcome
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>

--- pkg/engine/summary.go
+++ pkg/engine/summary.go
@@ -154,14 +154,11 @@ func (s *Summary) String() string {
 
 	b.WriteString("order:\n")
 	for _, record := range s.Records {
-		if record.Outcome == OutcomeSkipped {
-			continue
-		}
+		fmt.Fprintf(&b, "  %s: %s\n", record.Identity, record.Outcome)
+
 		if record.Diff == nil {
-			fmt.Fprintf(&b, "  %s\n", record.Identity)
 			continue
 		}
-		fmt.Fprintf(&b, "  %s:\n", record.Identity)
 		for line := range strings.Lines(record.Diff.String()) {
 			b.WriteString("    ")
 			b.WriteString(strings.TrimSuffix(line, "\n"))
